156e219e1fb3661f8cabde90584aef426031f668
Shubham Gupta <shubhamgmath@gmail.com>
2025-09-25 16:00:20 +0530

    Fixed menu opening wrong screen

--- src/router/customRouter.jsx
+++ src/router/customRouter.jsx
@@ -3,7 +3,6 @@ import Dashboard from '../views/Dashboard';
 import Settings from '../views/Settings';
 import Connections from '../views/Connections';
 import SetupWizard from '../views/SetupWizard';
-import ConnectionFailed from '../views/ConnectionFailed';
 import { routes } from '../admin/settings/routes';
 
 
@@ -12,8 +11,7 @@ const CustomRouter = () => {
   return (
     <Router routes={routes} defaultRoute={routes?.dashboard?.path}>
       <Route path={routes.dashboard.path}><Dashboard /></Route>
-      {/* <Route path={routes.settings.path}><Settings /></Route> */}
-      <Route path={routes.settings.path}><ConnectionFailed /></Route>
+      <Route path={routes.settings.path}><Settings /></Route>
       <Route path={routes.connection.path}><Connections /></Route>
       <Route path={routes.setupWizard.path}><SetupWizard /></Route>
     </Router>
